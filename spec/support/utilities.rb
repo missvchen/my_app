899d9677b4facda279a4af420f6ae146507ef879
Victoria <missvictoriachen@gmail.com>
2013-03-25 23:52:16 -0400

    Finish refactoring tests

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,9 +1,1 @@
-def full_title(page_title)
-  base_title = "Logiloans"
-  default_title = "Loans that make sense - Logiloans"
-  if page_title.empty?
-    default_title
-  else
-    "#{page_title} | #{base_title}"
-  end
-end
+include ApplicationHelper
