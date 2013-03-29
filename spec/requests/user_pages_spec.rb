186d43d10a275eaaee20eed1bd5baa0f94053e15
Victoria <missvictoriachen@gmail.com>
2013-03-29 13:13:44 -0400

    Finished adding tests for sign up

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,17 +28,39 @@ describe "UserPages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+      
+      describe "after submission" do
+        before { click_button submit }
+        
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+        it { should have_selector('div.alert.alert-error', content: 'error') }
+        it { should have_selector('li', text: "* Email can't be blank") }
+        it { should have_selector('li', text: "* Email is invalid") }
+        it { should have_selector('li', text: "* Password can't be blank") }
+        it { should have_selector('li', text: "* Password is too short (minimum is 6 characters)") }
+        it { should have_selector('li', text: "* Password confirmation can't be blank") }
+        it { should_not have_selector('li', text: "* Password digest can't be blank") }
+      end
     end
 
     describe "with valid information" do
       before do
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Password confirmation", with: "foobar"
       end
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+      
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+        
+        it { should have_selector('title', text: 'Account') }
+        it { should have_selector('div.alert.alert-success', text: 'Please check your email to complete your registration.')}
       end
     end
   end
