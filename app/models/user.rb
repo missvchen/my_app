ad342ea2f1d76cdbb32d348d7852e468e4b03f5f
Victoria <missvictoriachen@gmail.com>
2013-03-29 12:44:36 -0400

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,21 +12,21 @@
 
 class User < ActiveRecord::Base
   attr_accessible :display_name,
-                  :email,
                   :name,
+                  :email,
                   :password,
-                  :password_confirmation
+                  :password_confirmation,
   has_secure_password
   
   before_save { email.downcase! }
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
+  validates :display_name, length: { maximum: 150 }
   validates :email, presence: true,
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :name, presence: true,
-                   length: { maximum: 50, minimum: 2 }
+  validates :name, length: { maximum: 50 }
   validates :password, presence: true,
                        length: { minimum: 6 }
   validates :password_confirmation, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,11 +13,11 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name: "Example User",
-                            display_name: "E. User",
-                            email: "user@example.com",
+  before { @user = User.new(email: "user@example.com",
                             password: "foobar",
-                            password_confirmation: "foobar") }
+                            password_confirmation: "foobar",
+                            name: "Example User",
+                            display_name: "E. User") }
   
   subject { @user }
   
@@ -31,18 +31,8 @@ describe User do
   
   it { should be_valid }
   
-  describe "when name is not present" do
-    before { @user.name = " " }
-    it { should_not be_valid }
-  end
-  
   describe "when name is too long" do
     before { @user.name = 'a' * 51 }
-    it { should_not be_valid }
-  end
-  
-  describe "when name is too short" do
-    before { @user.name = 'a' }
     it { should_not be_valid }
   end
   
